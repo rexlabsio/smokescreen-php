238db43f46f60404c84304f7fad09ce2b3ba8a1b
Jodie Dunlop <jodie.dunlop@rexsoftware.com.au>
2018-04-11 17:55:01 +1000

    Add transformer resolution

---
+++ src/Transformer/TransformerResolverInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rexlabs\Smokescreen\Transformer;
+
+use Rexlabs\Smokescreen\Resource\ResourceInterface;
+
+interface TransformerResolverInterface
+{
+    /**
+     * Determines the Transformer object to be used for a particular resource.
+     *
+     * @param ResourceInterface $resource
+     * @return TransformerInterface|mixed|null
+     */
+    public function resolve(ResourceInterface $resource);
+}
